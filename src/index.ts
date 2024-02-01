d82eedba7da84e61de862e6f84ef2e28e43703b0
Patrice De Saint Steban <patrice@desaintsteban.fr>
2024-02-01 10:15:25 +0000

    Format code

--- src/index.ts
+++ src/index.ts
@@ -4,41 +4,41 @@ import Mailgun, { MailgunMessageData } from 'mailgun.js';
 import { cp } from 'fs';
 
 const mailgun = new Mailgun(FormData);
-const mailgunOptions = {username: 'api', key: process.env.MAILGUN_APIKEY || 'demo', url: "https://api.eu.mailgun.net"}
+const mailgunOptions = { username: 'api', key: process.env.MAILGUN_APIKEY || 'demo', url: "https://api.eu.mailgun.net" }
 const mg = mailgun.client(mailgunOptions);
 
 
 const run = async () => {
-    if (!process.env.PANIERBIO_LOGIN || !process.env.PANIERBIO_PASSWORD) {
-        console.error(`Must set env PANIERBIO_LOGIN and PANIERBIO_PASSWORD`)
-        return
-    }
-    let login :string = process.env.PANIERBIO_LOGIN
-    let pass :string = process.env.PANIERBIO_PASSWORD
-    let browser = await playwright.chromium.launch({headless: true});
-    const page = await browser.newPage();
-    await page.goto('https://www.lespaniersbiosolidaires.fr/')
-    console.log(await page.title())
-    await page.getByRole('link', { name: 'Connexion' }).click();
-    await page.waitForLoadState();
-    await page.locator('#client_login').fill(login);
-    await page.locator('#client_pwd').fill(pass);
-    await page.screenshot({ path: 'screen/login2.png' });
-    await Promise.all([
-        await page.waitForLoadState(),
-        await page.locator('#btn_login').click()
-    ])
-    await page.screenshot({ path: 'screen/login3.png' })
-    await page.waitForURL('https://www.lespaniersbiosolidaires.fr/espaceclient');
-    console.log(await page.title());
-    await page.screenshot({ path: 'screen/account.png' });
-    const titre = await page.locator('.pill').innerText()
-    console.log(titre)
-    const content = await page.locator('.compo-bloc')
-    const contentText = await content.innerText();
-    const contentHtml = await content.innerHTML();
-    console.log(content)
-    const text = `
+  if (!process.env.PANIERBIO_LOGIN || !process.env.PANIERBIO_PASSWORD) {
+    console.error(`Must set env PANIERBIO_LOGIN and PANIERBIO_PASSWORD`)
+    return
+  }
+  let login: string = process.env.PANIERBIO_LOGIN
+  let pass: string = process.env.PANIERBIO_PASSWORD
+  let browser = await playwright.chromium.launch({ headless: true });
+  const page = await browser.newPage();
+  await page.goto('https://www.lespaniersbiosolidaires.fr/')
+  console.log(await page.title())
+  await page.getByRole('link', { name: 'Connexion' }).click();
+  await page.waitForLoadState();
+  await page.locator('#client_login').fill(login);
+  await page.locator('#client_pwd').fill(pass);
+  await page.screenshot({ path: 'screen/login2.png' });
+  await Promise.all([
+    await page.waitForLoadState(),
+    await page.locator('#btn_login').click()
+  ])
+  await page.screenshot({ path: 'screen/login3.png' })
+  await page.waitForURL('https://www.lespaniersbiosolidaires.fr/espaceclient');
+  console.log(await page.title());
+  await page.screenshot({ path: 'screen/account.png' });
+  const titre = await page.locator('.pill').innerText()
+  console.log(titre)
+  const content = await page.locator('.compo-bloc')
+  const contentText = await content.innerText();
+  const contentHtml = await content.innerHTML();
+  console.log(content)
+  const text = `
 Mon panier ${titre}
 
 ${contentText}
@@ -47,7 +47,7 @@ ${contentText}
 Panier bio solidaire
     `
 
-    const html = `
+  const html = `
     <!doctype html>
     <html lang="en">
       <head>
@@ -371,12 +371,8 @@ Panier bio solidaire
             <td>&nbsp;</td>
             <td class="container">
               <div class="content">
-    
-                <!-- START CENTERED WHITE CONTAINER -->
                 <span class="preheader">Mon panier bio solidaire du ${titre}</span>
                 <table role="presentation" border="0" cellpadding="0" cellspacing="0" class="main">
-    
-                  <!-- START MAIN CONTENT AREA -->
                   <tr>
                     <td class="wrapper">
                     <h1>Mon panier ${titre}</h1>
@@ -385,24 +381,18 @@ Panier bio solidaire
                     </div>
                     </td>
                   </tr>
-    
-                  <!-- END MAIN CONTENT AREA -->
-                  </table>
-    
-                <!-- START FOOTER -->
+                </table>
                 <div class="footer">
                   <table role="presentation" border="0" cellpadding="0" cellspacing="0">
                     <tr>
                       <td class="content-block">
                         Panier bio solidaire
                       </td>
+                      <td>&nbsp;</td>
                     </tr>
                   </table>
                 </div>
-    
-                <!-- END FOOTER -->
-                
-    <!-- END CENTERED WHITE CONTAINER --></div>
+              </div>
             </td>
             <td>&nbsp;</td>
           </tr>
@@ -411,58 +401,58 @@ Panier bio solidaire
     </html>
         `
 
-    const data : MailgunMessageData = {
-        from: 'Panier bio solidaire <amp@todo.patou.dev>',
-        to: process.env.PANIERBIO_LOGIN,
-        subject: `Mon panier ${titre}`,
-        text,
-        html,
-        'o:dkim': true,
+  const data: MailgunMessageData = {
+    from: 'Panier bio solidaire <amp@todo.patou.dev>',
+    to: process.env.PANIERBIO_LOGIN,
+    subject: `Mon panier ${titre}`,
+    text,
+    html,
+    'o:dkim': true,
+  }
+  /*
+  const filename = titre.replace('Votre panier du ', "Croq'actus du ").replace(/ 01 /, " 1er ").replace(/ 0([2-9]) /, " $1 ")
+  // Si le titre n'est pas renseigné
+  if (filename.trim().length > 0) {
+    console.log(`Check ... ${filename}`)
+    const link = page.getByRole('link', { name: new RegExp(filename, 'gi') })
+    if (await link.count() > 0) {
+      console.log(`Download ... ${filename}`)
+      const [ download ] = await Promise.all([
+          // Start waiting for the download
+          page.waitForEvent('download'),
+          link.click()
+      ])
+      const path = await download.path();
+      console.log(path)
+      if (path) {
+          data.attachment = new mg.Attachment({
+              data: path,
+              filename: download.suggestedFilename(),
+          })
+          data.subject = filename
       }
-    /*
-    const filename = titre.replace('Votre panier du ', "Croq'actus du ").replace(/ 01 /, " 1er ").replace(/ 0([2-9]) /, " $1 ")
-    // Si le titre n'est pas renseigné
-    if (filename.trim().length > 0) {
-      console.log(`Check ... ${filename}`)
-      const link = page.getByRole('link', { name: new RegExp(filename, 'gi') })
-      if (await link.count() > 0) {
-        console.log(`Download ... ${filename}`)
-        const [ download ] = await Promise.all([
-            // Start waiting for the download
-            page.waitForEvent('download'),
-            link.click()
-        ])
-        const path = await download.path();
-        console.log(path)
-        if (path) {
-            data.attachment = new mg.Attachment({
-                data: path,
-                filename: download.suggestedFilename(),
-            })
-            data.subject = filename
-        }
-      }
     }
-    */
+  }
+  */
 
-    if (mailgunOptions.key !== 'demo' && process.env.SEND_EMAIL === 'yes') {
-      logEmail("Envoi de l'email", data);
-      await mg.messages.create('todo.patou.dev', data);
-    }
-    else {
-      logEmail('Email non envoyé', data);
-    }
+  if (mailgunOptions.key !== 'demo' && process.env.SEND_EMAIL === 'yes') {
+    logEmail("Envoi de l'email", data);
+    await mg.messages.create('todo.patou.dev', data);
+  }
+  else {
+    logEmail('Email non envoyé', data);
+  }
 
-    await browser.close();
+  await browser.close();
 };
 run();
 
-function logEmail(msg : string, data : MailgunMessageData) {
+function logEmail(msg: string, data: MailgunMessageData) {
   console.log(`${msg} :
 à: ${data['to']}
 sujet: ${data['subject']}
   
 ${data['text']?.replaceAll('\\n', '\n')}
-${data['attachment'] ? `attachement: ${(data['attachment'] as any).filename}`: ''}
+${data['attachment'] ? `attachement: ${(data['attachment'] as any).filename}` : ''}
 `);
 }
